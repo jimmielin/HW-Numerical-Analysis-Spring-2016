3998ce73971bf9d8df3b4bf9905c06ff4d467ad1
Jimmie Lin <myself@jimmielin.me>
2016-04-02 18:20:51 +0800

    Jacobi Method (DDLE).
    Jacobi method is an algorithm for determining the solutions of a
    diagonally dominant system of linear equations.

---
+++ 20160402-matrices/jacobi.hs
@@ -0,0 +1,30 @@
+-- Jacobi Iterative Method for solving Linear Equation System
+-- April 2, 2016
+import Data.Matrix
+
+-- |Jacobi Iterative Method for solving Linear Equation System Ax=b
+jacobisolve :: (RealFloat a, Enum a) => Matrix a                      -- ^ A square marix with **nonzero** diagonals
+                                     -> Matrix a                      -- ^ b column vector, matrix form
+                                     -> Int                           -- ^ number of iterations applied
+                                     -> Matrix a                      -- ^ solution x
+jacobisolve a b n = jacobisolve_int j (zero (nrows a) 1) dib n where
+    (di, l, u) = diludecomp a
+    j = multStd2 di $ l + u
+    dib = multStd2 di b
+
+diludecomp :: (RealFloat a, Enum a) => Matrix a                       -- ^ A square matrix with **nonzero** diagonals
+                                    -> (Matrix a, Matrix a, Matrix a) -- ^ (D-1, L, U) matrices
+diludecomp m = (di, l, u) where
+    n  = nrows m
+    di = matrix n n (\(x, y) -> if x == y then 1 / (m ! (x, x)) else 0)
+    l  = matrix n n (\(x, y) -> if x > y then m ! (x, y) * (-1) else 0)
+    u  = matrix n n (\(x, y) -> if x < y then m ! (x, y) * (-1) else 0)
+
+jacobisolve_int :: (RealFloat a, Enum a) => Matrix a                  -- ^ Jacobi Iterative Matrix J = D^(-1) * (L + U)
+                                         -> Matrix a                  -- ^ x_i row vector (matrix form) for solution estimation
+                                         -> Matrix a                  -- ^ D^(-1) * b
+                                         -> Int                       -- ^ number of runs remaining.
+                                         -> Matrix a                  -- ^ x_i row vector solution estimation
+jacobisolve_int _ x _ 0   = x
+jacobisolve_int j x dib n = jacobisolve_int j xn dib $ if xn == x then 0 else n - 1 where
+    xn = (+) dib $ multStd2 j x
